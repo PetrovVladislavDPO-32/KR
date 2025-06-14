a5e5abc92b56cd2a191d8cc189355cb420a80413
petrov <petrov@vbox.Dlink>
2025-06-14 13:16:25 +0800

    Test CI/CD

--- auth/accounts/tests.py
+++ auth/accounts/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class AuthTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='test', password='testpass123')
+        self.assertEqual(user.username, 'test')
--- tasks/taskapp/tests.py
+++ tasks/taskapp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskTests(TestCase):
+    def test_create_task(self):
+        task = Task.objects.create(description="Test task", status="new")
+        self.assertEqual(task.status, "new")
